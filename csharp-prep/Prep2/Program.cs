99f2690d2d74ed496981d3a0913ca2afc670c49d
Elsie Essien <arikpoelsie02@gmail.com>
2024-03-24 02:46:32 +0100

    WK1 prep assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int percentage = int.Parse(response);
+
+        string letterGrade = "";
+
+        int gradeScale;
+        gradeScale =  percentage % 10;
+
+        string sign;
+        sign = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (gradeScale >= 7)
+        {
+            sign = "+";
+        }
+        else if (gradeScale < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }        
+
+        if (letterGrade == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        if (letterGrade == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulation, You passed this course!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed this Course. ");
+            Console.WriteLine("Please try harder next time!");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userInput = -1;
+        while (userInput != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+
+            string userResponse = Console.ReadLine();
+            userInput = int.Parse(userResponse);
+
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        // Part 1: Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
+    
